2115c6104bbdc0e860b08ff03419f3bb438851af
Sean Szurko <sean_szurko@yahoo.com>
2016-10-15 22:31:14 -0700

    Minor refactor of two_sum algorithm

--- two_sum_algorithm.rb
+++ two_sum_algorithm.rb
@@ -3,8 +3,8 @@ def two_sum(nums, target)
   numbers_hash = {}
   nums.each_with_index do |number, index|
     numbers_hash[index] = number
-    key_indicies = ((numbers_hash.select { |k,v| v == (target - number) }.keys) - [index])
-    key_indicies.empty? ? false : (return [numbers_hash.key(target - number), index].flatten)
+    hash_key = ((numbers_hash.select { |k,v| v == (target - number) }.keys) - [index])
+    hash_key.empty? ? false : (return [hash_key, index].flatten)
   end
   return "Couldn't find target value"
 end
