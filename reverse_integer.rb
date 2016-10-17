192366937b4b74f3b04bcb8fed5a3a64805bcc2f
Sean Szurko <sean_szurko@yahoo.com>
2016-10-16 20:20:40 -0700

    add reverse_integer algorithm solution

---
+++ reverse_integer.rb
@@ -0,0 +1,16 @@
+# Reverse digits of an integer.
+#
+# Example1: x = 123, return 321
+# Example2: x = -123, return -321
+# Must take into account possible stack overflows (even though it is Ruby)
+
+def reverse(x)
+  number = x.to_s.gsub(/[^\d]/, '')
+  sign  = x.to_s.gsub(/[^\+|\-]/, '')
+  reversed_number = (sign + number.to_s.split("").reverse.join("")).to_i
+  if reversed_number > 2**31 - 1 || reversed_number < -2**31
+    0
+  else
+    reversed_number
+  end
+end
